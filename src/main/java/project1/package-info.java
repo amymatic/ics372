f8c564668a7cc457060d611fb678354bf63ec713
amymatic <amymatic@gmail.com>
2020-02-11 17:14:10 -0600

    Create package-info.java

---
+++ src/main/java/project1/package-info.java
@@ -0,0 +1,8 @@
+/**
+* The project1 package is a set of classes for the first deliverable
+* of Group Assignment 1 for ICS 372 Spring 2020 at Metro State University.
+* 
+* @version 1.0
+* @author Amy Bell, Xavier Jackson, Jigme Ritzeku, Daniel Ramage
+*/
+package project1;
